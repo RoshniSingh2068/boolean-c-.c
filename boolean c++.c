3766e235408aee1f2a0aa35ec2c9f98d32773df7
RoshniSingh2068 <roshnisingh1228@gmail.com>
2024-08-26 10:13:03 +0530

    Create boolean c++.c

---
+++ boolean c++.c
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std ;
+
+int  main (){
+    bool isCoadingFun =true;
+    bool isFishTasty = true;
+    bool isStudyBorning = false;
+    cout <<  isCoadingFun << "\n";
+    cout <<  isFishTasty << "\n";
+    cout << isStudyBorning  << "\n";
+    return 0;
+
+}
